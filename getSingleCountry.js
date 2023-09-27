46bbe326dd7ec4648a95f537c64d36ba6ff2f20e
code-emad <emadul.islam@hotmail.co.uk>
2023-09-27 13:45:29 +0100

    tidy up

--- getSingleCountry.js
+++ getSingleCountry.js
@@ -1,32 +1,25 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
-const fs = require('fs/promises'); // Import the fs module with promises support
+const fs = require('fs/promises');
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-
   // Navigate to the URL
   await page.goto('https://climateaction.unfccc.int/apiv2/actor/country/USA');
-
   // Extract the page content
   const pageContent = await page.content();
-
   // Load the HTML content into Cheerio
   const $ = cheerio.load(pageContent);
-
   // Find the <pre> element containing JSON data
   const jsonText = $('pre').text().trim();
-
   // Check if the JSON data is not empty
   if (jsonText) {
     try {
       // Parse the JSON data into a JavaScript object
       const jsonData = JSON.parse(jsonText);
-
       // Write the response data to a JSON file
-      await fs.writeFile('responseUSA.json', JSON.stringify(jsonData, null, 2));
-
+      await fs.writeFile('singleCountry.json', JSON.stringify(jsonData, null, 2));
       console.log('JSON Data saved to response.json');
     } catch (error) {
       console.error('Error parsing JSON:', error);
@@ -34,7 +27,6 @@ const fs = require('fs/promises'); // Import the fs module with promises support
   } else {
     console.error('No JSON data found in the response.');
   }
-
   // Close the browser
   await browser.close();
 })();
